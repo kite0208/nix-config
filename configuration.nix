9cb36d66580285d9ca6f36978c539fe26eb6fc8f
kite0208 <92876660+kite0208@users.noreply.github.com>
2022-06-14 16:32:42 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,30 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
+environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
+  ...
+  services.xserver = {
+    enable = true;
 
+    desktopManager = {
+      xterm.enable = false;
+    };
+   
+    displayManager = {
+        defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+     ];
+    };
+  };
+  ...
+}
 {
   imports =
     [ # Include the results of the hardware scan.
